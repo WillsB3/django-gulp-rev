b25d106e488c650dd522cccf0d7680fe2bb8a0fe
Wills Bithrey <wills@willsbithrey.com>
2019-08-07 14:27:31 +0100

    Cleanup double @override_settings

--- gulp_rev/tests/tests.py
+++ gulp_rev/tests/tests.py
@@ -23,8 +23,7 @@ class gulp_revTestCase(TestCase):
     def test_is_use_versioned_assets_true(self):
         self.assertTrue(gulp_rev.use_versioned_assets())
 
-    @override_settings(DEBUG=True)
-    @override_settings(GULP_REV_USE_VERSIONED_ASSETS=True)
+    @override_settings(DEBUG=True, GULP_REV_USE_VERSIONED_ASSETS=True)
     def test_ignores_debug_if_use_versioned_assets_specified(self):
         self.assertTrue(gulp_rev.use_versioned_assets())
 
